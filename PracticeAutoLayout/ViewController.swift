886a51e5a91bb4e239bb9882e37b91d3d5626613
홍정민 <dream7739@naver.com>
2024-05-20 23:57:15 +0900

    배민 오토레이아웃 실습

--- PracticeAutoLayout/ViewController.swift
+++ PracticeAutoLayout/ViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttonList: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for btn in buttonList {
+            btn.layer.cornerRadius = 5
+            btn.layer.borderWidth = 2
+            btn.layer.borderColor = UIColor.black.cgColor
+        }
         // Do any additional setup after loading the view.
     }
 
